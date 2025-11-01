b48ee78608f5419ebcc1de61e041cd5d0b42bd49
bvganesh7794 <bvganesh7794@gmail.com>
2025-11-01 17:07:07 +0530

    Initial project structure and main class

---
+++ src/main/java/com/seaprobe/SeaProbeApplication.java
@@ -0,0 +1,11 @@
+package com.seaprobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaProbeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SeaProbeApplication.class, args);
+    }
+}
